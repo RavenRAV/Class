f70104406ba5d270abf493de3da4761b90a48cc5
RavenRav <akilaia@inbox.ru>
2019-07-06 00:01:37 +0300

    :|

--- src/com/company/Door.java
+++ src/com/company/Door.java
@@ -1,12 +1,12 @@
 package com.company;
 
 public class Door {
-    public Door[] generateHero(){
-        Door[] heArr = new Door[4];
-        Door warrior = new Door();
-        Door magician = new Door();
-        Door magicianHil = new Door();
-        Door kinetik = new Door();
+    public Hero[] generateHero(){
+        Hero[] heArr = new Hero[4];
+        Hero warrior = new Hero(250,20,1);
+        Hero magician = new Hero(230,25,2);
+        Hero magicianHil = new Hero(200,10,3);
+        Hero kinetik = new Hero(240,24,2);
         heArr[0] = warrior;
         heArr[1] = magician;
         heArr[2] = magicianHil;
